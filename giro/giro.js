e3cad86b890e1f73ad8bc22f80a831792662386d
alexbuse <30476527+alexbuse@users.noreply.github.com>
2019-10-18 15:27:07 +0300

    Add files via upload

--- giro/giro.js
+++ giro/giro.js
@@ -4,5 +4,5 @@ function on_rotate_uab(e)
 {
 	document.getElementById("id_alpha").innerHTML = e.alpha;
 	document.getElementById("id_beta").innerHTML = e.beta;
-	document.getElementById("id_gamma").innerHTML = e.gama;
+	document.getElementById("id_gamma").innerHTML = e.gamma;
 }	
