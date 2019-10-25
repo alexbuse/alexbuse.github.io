0c02d967c3d18db4ca53fe79a59444cf0c143c60
alexbuse <30476527+alexbuse@users.noreply.github.com>
2019-10-25 14:14:00 +0300

    Add files via upload

--- giro/giro.js
+++ giro/giro.js
@@ -9,9 +9,9 @@ window.addEventListener("devicemotion",on_motion_uab);
 
 function on_orientation_uab(e)
 {
-	document.getElementById("id_alpha").innerHTML = Math.round((e.alpha*100)/100);
-	document.getElementById("id_beta").innerHTML = Math.round((e.beta*100)/100);
-	document.getElementById("id_gamma").innerHTML = Math.round((e.gamma*100)/100);
+	document.getElementById("id_alpha").innerHTML = Math.round(e.alpha*100)/100;
+	document.getElementById("id_beta").innerHTML = Math.round(e.beta*100)/100;
+	document.getElementById("id_gamma").innerHTML = Math.round(e.gamma*100)/100;
 }	
 
 function on_motion_uab(e)
