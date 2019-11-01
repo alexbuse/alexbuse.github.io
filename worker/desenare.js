1daccb5b295a8e6df4c2a65812d6b2beb8fdcceb
alexbuse <30476527+alexbuse@users.noreply.github.com>
2019-11-01 14:35:28 +0200

    Add files via upload

--- worker/desenare.js
+++ worker/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML = "Logic version = 2019.11.1.2";
+document.getElementById("id_logic").innerHTML = "Logic version = 2019.11.1.3";
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
 //se declara un timer global pentru a putea fi apelat de functii
@@ -33,6 +33,11 @@ function start()
 	document.getElementById("id_stop").disabled = false;
 	var canvas = document.getElementById("id_canvas");
 	var context = canvas.getContext("2d");
+
+	var muncitor = new Worker("calcul_prime.js");
+	muncitor.onmessage = function(e){
+		document.getElementById("id_prime").innerHTML = e.data;
+	}
 
 	var raza_mare = 100;
 	var raza_mica = 10;
