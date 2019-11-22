38ca0b11c4519544cb622d0c20250ba5ce2a00e0
alexbuse <30476527+alexbuse@users.noreply.github.com>
2019-11-22 14:29:27 +0200

    Add files via upload

---
+++ cam/cam.js
@@ -0,0 +1,18 @@
+document.getElementById("id_logic_version").innerHTML = "2019.11.22.0";
+
+var c = { audio: true, video: true };
+
+
+var video = document.getElementById("id_video");
+
+navigator.mediaDevices.getUserMedia(c).then(on_ok_cam_uab).catch(on_fail_cam_uab);
+
+function on_ok_cam_uab(e)
+{
+	video.srcObject = e;
+}
+
+function on_fail_cam_uab(e)
+{
+	alert("failure "+e);
+}
