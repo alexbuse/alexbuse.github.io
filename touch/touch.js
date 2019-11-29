8022240c3b677fb4948b0c6c006f4a8cd8c8d060
alexbuse <30476527+alexbuse@users.noreply.github.com>
2019-11-29 13:22:42 +0200

    Add files via upload

--- touch/touch.js
+++ touch/touch.js
@@ -7,6 +7,16 @@ var context = canvas.getContext("2d");
 
 var client_rect = canvas.getBoundingClientRect();
 
+function get_random_color()
+{
+	var tmp = "0123456789ABCDEF";
+	var culoare = "#";
+	for (var i = 0; i < 6 ; i++)
+		culoare += tmp[Math.floor(Math.random() * 16)];
+	return culoare;
+	
+}
+
 function touch_start_uab(e)
 {
 	var t = e.changedTouches;
@@ -14,6 +24,8 @@ function touch_start_uab(e)
 	{
 		context.beginPatch();
 		context.arc(t[i].pageX - client_rect.left, t[i].pageY - client_rect.top, 10, 0, 2 * Math.PI);
+		context.fillStyle = get_random_color();
+		context.strokeStyle = context.fillStyle;
 		context.stroke();
 	}
 }
